8255338d06a2ee8aa53749c6bedeefb6de8eb694
Devsaider <sebasmoraresu@gmail.com>
2022-05-12 00:22:16 -0500

    fix typo

--- commands/general/help.js
+++ commands/general/help.js
@@ -6,7 +6,7 @@ module.exports = {
     const chat = await msg.getChat();
 
     chat.sendMessage(
-      `🤖 *WServant v${botVersion} — asistente multipropósito y utilidad*\n🧾 *Lista de comandos*  :  https://gist.github.com/nzkdevsaider/ba41879cdc0b7975f744948b95de2523 \n🗃️ *GitHub*  :  https://github.com/nzkdevsaider/wservant \n\n⚠ *WServant no almacena ningún mensaje, imagen, audio, vídeo y/o información de contactos. Ningún tipo de almacenamiento de información personal está estipulado en su código fuente y no se comparte con terceros. Su privacidad es nuestra prioridad.*`
+      `🤖 *WServant v${botVersion} — asistente multipropósito y utilidad*\n\n🧾 *Lista de comandos*  :  https://gist.github.com/nzkdevsaider/ba41879cdc0b7975f744948b95de2523 \n🗃️ *GitHub*  :  https://github.com/nzkdevsaider/wservant \n\n⚠ *WServant no almacena ningún mensaje, imagen, audio, vídeo y/o información de contactos.* Ningún tipo de almacenamiento de información personal está estipulado en su código fuente y no se comparte con terceros. Su privacidad es nuestra prioridad.`
     );
   },
 };
