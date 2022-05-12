f8ed1fdca719f54882d5ff0683edc7c92d5bb8b0
Devsaider <sebasmoraresu@gmail.com>
2022-05-12 00:00:21 -0500

    damn

--- commands/general/help.js
+++ commands/general/help.js
@@ -3,7 +3,7 @@ module.exports = {
 
   execute(msg, args, client) {
     const { botVersion } = client;
-    const chat = msg.getChat();
+    const chat = await msg.getChat();
 
     chat.sendMessage(
       `🤖 *WServant v${botVersion} — asistente multipropósito y utilidad*\n🧾 *Lista de comandos*  :  https://gist.github.com/nzkdevsaider/ba41879cdc0b7975f744948b95de2523 \n🗃️ *GitHub*  :  https://github.com/nzkdevsaider/wservant \n\n⚠ *WServant no almacena ningún mensaje, imagen, audio, vídeo y/o información de contactos. Ningún tipo de almacenamiento de información personal está estipulado en su código fuente y no se comparte con terceros. Su privacidad es nuestra prioridad.*`
