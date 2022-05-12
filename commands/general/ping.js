2b077f5b378fa2fe318e7768a1c88c1cb8ceef50
Devsaider <sebasmoraresu@gmail.com>
2022-05-11 23:57:59 -0500

    ping

--- commands/general/ping.js
+++ commands/general/ping.js
@@ -4,13 +4,15 @@ const { version, dependencies } = require("./../../package.json");
 module.exports = {
   name: "ping",
   description: "Latencia y estado del bot.",
-  
+
   execute(msg, args, client) {
     let emojis = ["💅", "✊", "👍", "🖐", "🤙", "🤟"];
+    let time = new Date(msg.timestamp * 1000);
+
     msg.reply(
       `${random(emojis)} *v${version}* (engine ${
         dependencies["whatsapp-web.js"]
-      }) - *${new Date() - client.botStart}ms*`
+      }) - *${new Date() - time}ms*`
     );
   },
 };
